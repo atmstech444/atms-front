e87ad0ffd86428951b2a724d8384d2eb27014544
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-11-20 05:42:18 -0800

    style:career page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Header } from "./components/Header";
 import AboutUs from "./pages/AboutUs";
-import Career from "./pages/Career";
+import Career from "./pages/career/Career";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import Services from "./pages/Services";
